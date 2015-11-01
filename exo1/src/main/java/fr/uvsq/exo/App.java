0966dcbcfd3e05d12da0f779542c76e06e34037c
hanane ouksili <hanane ouksili>
2015-11-01 19:55:56 +0100

    add exo1

---
+++ exo1/src/main/java/fr/uvsq/exo/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.exo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
